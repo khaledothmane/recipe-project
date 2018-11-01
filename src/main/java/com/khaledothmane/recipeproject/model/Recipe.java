24f33df0facad666de90fb15034d430e8d21b931
Khaled Othmane <khaled.othmane@gmail.com>
2018-11-01 22:27:33 +0000

    Adding Category Model. Closes #6

--- src/main/java/com/khaledothmane/recipeproject/model/Recipe.java
+++ src/main/java/com/khaledothmane/recipeproject/model/Recipe.java
@@ -18,6 +18,11 @@ public class Recipe {
     private String url;
     private String directions;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
