6f05608a58bcb5f5e9016a8f166f5f05258fc74d
Khaled Othmane <khaled.othmane@gmail.com>
2018-11-04 00:38:18 +0000

    Database initialisation. Closes #8

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (name) VALUES('American');
+INSERT INTO category (name) VALUES('Mexican');
+INSERT INTO unit_of_measure (uom) VALUES('Teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES('Piston');
